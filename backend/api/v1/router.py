c5a141af0b876b67170df8f50079c8dda1199234
Woolfer0097 <Woolfer0097@yandex.ru>
2026-04-01 13:59:17 +0300

    chore: remove redundant template

--- backend/api/v1/router.py
+++ backend/api/v1/router.py
@@ -1,8 +1,7 @@
 from fastapi import APIRouter
 
-from backend.api.v1.endpoints import generate, health, items
+from backend.api.v1.endpoints import generate, health
 
 router = APIRouter()
 router.include_router(health.router)
-router.include_router(items.router)
 router.include_router(generate.router)
